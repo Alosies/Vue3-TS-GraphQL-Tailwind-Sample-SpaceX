173786efad94b61a29106e7fd407ee5d169b60c3
Alosies George <alosies@gmail.com>
2022-04-09 13:24:38 +0530

    set base path in vite

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Vue3-TS-GraphQL-Tailwind-Sample-SpaceX/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
