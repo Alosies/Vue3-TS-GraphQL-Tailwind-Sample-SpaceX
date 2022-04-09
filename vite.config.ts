b28f7fe2883dd6eb9abacbe6ffe696ba0680e0dd
Alosies George <alosies@gmail.com>
2022-04-09 14:54:54 +0530

    remove base path

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Vue3-TS-GraphQL-Tailwind-Sample-SpaceX/',
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
