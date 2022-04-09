eb0217b32a7eabe131f81edd23d9df304680bae7
Alosies George <alosies@gmail.com>
2022-04-09 13:10:51 +0530

    Keep routes alive

--- src/components/layout/LayoutMain.vue
+++ src/components/layout/LayoutMain.vue
@@ -27,7 +27,11 @@
       </div>
     </nav>
     <main class="flex justify-center">
-      <router-view />
+      <router-view v-slot="{ Component }">
+        <keep-alive>
+          <component :is="Component" />
+        </keep-alive>
+      </router-view>
     </main>
   </div>
 </template>
